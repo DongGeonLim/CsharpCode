1ad70cad16a4b487a2bfdeea394fedf765887821
amadong <86343697+DongGeonLim@users.noreply.github.com>
2021-08-24 16:15:38 +0900

    Add files via upload

---
+++ func_params.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class Program
+    {
+        static int Total(params int[] values)
+        {
+            int total = 0;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                total += values[i];
+            }
+            return total;
+        }
+
+        static void CheckValues(params object[] values)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine("Value: {0}  {1}", values[i], values[i].GetType());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Total: " + Total(200, 5, 3, 1, 100));
+            Console.WriteLine("Total: " + Total(1, 2, 3, 4, 5));
+            Console.WriteLine("Total: " + Total(10, 1000));
+
+            CheckValues(0, 10.2f, 10.4, 10434839840d, "해위");
+        }
+    }
+}
