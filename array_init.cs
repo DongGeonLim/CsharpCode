b3038aedaad6f25d5894b21f8b72f271d999aed7
amadong <86343697+DongGeonLim@users.noreply.github.com>
2021-09-10 14:24:58 +0900

    Add files via upload

---
+++ array_init.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrNum = new int[3];
+
+            arrNum[0] = 1;
+            arrNum[1] = 2;
+            arrNum[2] = 3;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("  {0}", arrNum[i]);
+            }
+
+            Console.WriteLine("\narrNum.GetType():  {0}", arrNum.GetType());
+        }
+    }
+}
